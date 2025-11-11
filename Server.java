c3758776bcc6246af174e94374c631f90bfadffb
DungDao23092005 <dungdao108@gmail.com>
2025-11-11 20:42:00 +0700

    update 1

--- Server.java
+++ Server.java
@@ -16,13 +16,11 @@ public class Server {
             while (true) {
                 Socket player1 = serverSocket.accept();
                 System.out.println("Nguoi choi 1 da ket noi: " + player1.getInetAddress());
-                PrintWriter out1 = new PrintWriter(player1.getOutputStream(), true);
-                out1.println("CHO");
                 Socket player2 = serverSocket.accept();
                 System.out.println("Nguoi choi 2 da ket noi: " + player2.getInetAddress());
                 System.out.println("Da du 2 nguoi choi. Bat dau GameHandler...");
                 GameHandler gameHandler = new GameHandler(player1, player2);
-                gameHandler.start();
+                gameHandler.start(); // Chay Thread
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -41,48 +39,79 @@ class GameHandler extends Thread {
 
     @Override
     public void run() {
-        try {
+        try (
             BufferedReader in1 = new BufferedReader(new InputStreamReader(player1.getInputStream()));
             PrintWriter out1 = new PrintWriter(player1.getOutputStream(), true);
             BufferedReader in2 = new BufferedReader(new InputStreamReader(player2.getInputStream()));
-            PrintWriter out2 = new PrintWriter(player2.getOutputStream(), true);
-            out1.println("START");
-            out2.println("START");
+            PrintWriter out2 = new PrintWriter(player2.getOutputStream(), true)
+        ) {
+            out1.println("CHO");
+            while (true) {
+                System.out.println("Gui tin hieu START cho vong moi...");
+                out1.println("START");
+                out2.println("START");
+                String choice1 = in1.readLine();
+                String choice2 = in2.readLine();
+                if (choice1 == null) {
+                    System.out.println("Nguoi choi 1 da thoat.");
+                    try { out2.println("QUIT_DOI_THU"); } catch (Exception e) {}
+                    break;
+                }
+                if (choice2 == null) {
+                    System.out.println("Nguoi choi 2 da thoat.");
+                    try { out1.println("QUIT_DOI_THU"); } catch (Exception e) {}
+                    break;
+                }
+                
+                System.out.println("P1 chon: " + choice1 + ", P2 chon: " + choice2);
+                String result1 = "";
+                String result2 = "";
+                if (choice1.equals(choice2)) {
+                    result1 = "HOA";
+                    result2 = "HOA";
+                } else if ((choice1.equals("KEO") && choice2.equals("BAO")) ||
+                           (choice1.equals("BUA") && choice2.equals("KEO")) ||
+                           (choice1.equals("BAO") && choice2.equals("BUA"))) {
+                    result1 = "THANG";
+                    result2 = "THUA";
+                } else {
+                    result1 = "THUA";
+                    result2 = "THANG";
+                }
+                
+                System.out.println("-> Ket qua: P1 " + result1 + ", P2 " + result2);
 
-            String choice1 = in1.readLine();
-            System.out.println("Nguoi choi 1 chon: " + choice1);
-
-            String choice2 = in2.readLine();
-            System.out.println("Nguoi choi 2 chon: " + choice2);
-            String result1 = "";
-            String result2 = "";
-
-            if (choice1.equals(choice2)) {
-                result1 = "HOA";
-                result2 = "HOA";
-            } else if ((choice1.equals("KEO") && choice2.equals("BAO")) ||
-                       (choice1.equals("BUA") && choice2.equals("KEO")) ||
-                       (choice1.equals("BAO") && choice2.equals("BUA"))) {
-                result1 = "THANG";
-                result2 = "THUA";
-            } else {
-                result1 = "THUA";
-                result2 = "THANG";
+                out1.println(result1 + ":" + choice1 + ":" + choice2);
+                out2.println(result2 + ":" + choice2 + ":" + choice1);
+                out1.println("CHOI_TIEP?");
+                out2.println("CHOI_TIEP?");
+                
+                String playAgain1 = in1.readLine();
+                String playAgain2 = in2.readLine();
+                if (playAgain1 == null || playAgain2 == null) {
+                     System.out.println("Mot nguoi choi da thoat khi dang hoi choi lai.");
+                     try { out1.println("QUIT_DOI_THU"); } catch (Exception e) {}
+                     try { out2.println("QUIT_DOI_THU"); } catch (Exception e) {}
+                     break;
+                }
+                if (!playAgain1.equals("Y") || !playAgain2.equals("Y")) {
+                    System.out.println("Game ket thuc do nguoi choi chon thoat.");
+                    out1.println("QUIT_GAME");
+                    out2.println("QUIT_GAME");
+                    break;
+                }
+                
+                System.out.println("Ca hai deu dong y choi tiep.");
             }
-            
-            System.out.println("-> Ket qua: P1 " + result1 + ", P2 " + result2);
-            out1.println("KET QUA: BAN " + result1);
-            out2.println("KET QUA: BAN " + result2);
 
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("Loi trong GameHandler: " + e.getMessage());
         } finally {
             try {
                 player1.close();
                 player2.close();
-                System.out.println("Da dong ket noi 1 game session.");
+                System.out.println("Da dong ket noi cho 1 game session.");
             } catch (Exception e) {
-                e.printStackTrace();
             }
         }
     }
